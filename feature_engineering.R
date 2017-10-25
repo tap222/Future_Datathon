26132a45fb58132e8ce8bb253204912ac787a9d1
tap222 <mtapas.mohanty95@gmail.com>
2017-10-26 03:43:02 +0530

    Create feature_engineering.R

---
+++ feature_engineering.R
@@ -0,0 +1,19 @@
+library(lubridate)
+library(dplyr)
+products$DOB<-ymd(products$DOB)
+products$DOB_year<-year(products$DOB)
+products$DOB_year<-as.factor(products$DOB_year)
+products$trans_year <- year(products$transactionDate)
+products$trans_year <- as.factor(products$trans_year)
+products<-products %>% group_by(Gender) %>% mutate(gen_sum = sum(sale_price_after_promo))
+products<-products %>% group_by(State) %>% mutate(State_sum = sum(sale_price_after_promo))
+products<-products %>% group_by(discountUsed) %>% mutate(discount_sum = sum(sale_price_after_promo))
+products<-products %>% group_by(promo_code) %>% mutate(promo_code_sum = sum(sale_price_after_promo))
+products<-products %>% group_by(trans_year) %>% mutate(trans_year_sum = sum(sale_price_after_promo))
+products<-products %>% group_by(DOB_year) %>% mutate(DOB_year_sum = sum(sale_price_after_promo))
+products$trans_month<- month(products$transactionDate)
+
+
+
+
+ 
